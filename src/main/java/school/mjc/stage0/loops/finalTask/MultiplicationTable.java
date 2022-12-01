f17a5647b231a51cf6d0880bb2f123d78e89ef07
khomess <sasha.khomenko12@gmail.com>
2022-12-01 14:11:53 +0200

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        for (int x = 1; x <= 10; x++){
+            System.out.println(x+" x "+numberTableToPrint+" = "+x*numberTableToPrint);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int x = 1;
+        for (int y = 1; y <= power; y++){
+            x *= numberToPrint;
+        }
+        System.out.println(x);
     }
 
     public static void main(String[] args) {
